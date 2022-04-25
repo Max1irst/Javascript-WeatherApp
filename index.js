34d23181b76d2ae793144d93eb1b77846058a719
Max Bobrov <maxbobrov1997@gmail.com>
2022-04-25 16:10:00 +0300

    worked with api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const btnSearch = document.querySelector(".searcher button")
 
 btnSearch.addEventListener("click", async function fetchAsync(){
     const cityName = document.querySelector(".searcher input").value 
-    const server = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=cddb3309931925c9b79ddc5a03842829`
+    const server = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${ApiKey}`
     const response = await fetch(server)
     const data = response.json()
     console.log(data);
