498bc9f1c090fc362b7ac57ca1acca7782682450
Max Bobrov <maxbobrov1997@gmail.com>
2022-04-26 19:22:59 +0300

    js

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ const btnSearch = document.querySelector(".searcher button")
 
 async function fetchAsync(){
     const cityName = document.querySelector(".searcher input").value 
-    const server = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${ApiKey}`
-    const response = await fetch(server)
-    const data = response.json()
-    console.log(data);
+    const server = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},uk&appid=${ApiKey}`
+    fetch(server)
+    .then (function(response){return response.json()})
+    .then(function(data){
+        console.log(data);
+        city.textContent = data.name
+    })
     document.querySelector(".searcher input").value = ""
 }
 btnSearch.addEventListener("click", fetchAsync)
