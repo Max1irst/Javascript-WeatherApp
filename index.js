6e7c57080621e51d9799119dd678278a8a187429
Max Bobrov <maxbobrov1997@gmail.com>
2022-04-26 19:02:06 +0300

    debug

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const city = document.querySelector(".city")
+const weather = document.querySelector(".weather")
+const img = document.querySelector(".img")
+const comment = document.querySelector(".comment")
+
+
+const ApiKey = 'cddb3309931925c9b79ddc5a03842829'
 const btnSearch = document.querySelector(".searcher button")
 
-btnSearch.addEventListener("click", async function fetchAsync(){
+async function fetchAsync(){
     const cityName = document.querySelector(".searcher input").value 
-    const server = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${ApiKey}`
+    const server = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${ApiKey}`
     const response = await fetch(server)
     const data = response.json()
     console.log(data);
     document.querySelector(".searcher input").value = ""
-})
+}
+btnSearch.addEventListener("click", fetchAsync)
